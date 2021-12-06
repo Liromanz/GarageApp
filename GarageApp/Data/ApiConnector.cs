f20228b5dfdea9824674f9dfdaa10b4c19dadc3f
NackBard <artem.guskov.03@mail.ru>
2021-12-06 12:03:33 +0300

    Добавил взаимодействие приложения с API

---
+++ GarageApp/Data/ApiConnector.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageApp.Data
+{
+    internal class ApiConnector : DataSender
+    {
+        public static async Task<ObservableCollection<T>> GetAll<T>(string tableName)
+        {
+            string responseBody = await GetRequest(tableName);
+            return (ObservableCollection<T>)JsonConvert.DeserializeObject(responseBody, typeof(ObservableCollection<T>));
+        }
+    }
+}
